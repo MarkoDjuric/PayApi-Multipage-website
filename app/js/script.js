b73b49b2de74c4fadd4a89718a4649529ddf3735
Marko djuric <maximumspid@gmail.com>
2022-05-05 10:05:02 +0200

    styling

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,14 @@
-console.log('HELLO');
+const navToggle = document.querySelector('.header__toggle');
+const nav = document.querySelector('.nav-side');
+const hamburger = document.querySelector('.hamburger');
+let hamBef = window.getComputedStyle(hamburger, '::before');
+let color = style.getPropertyValue('background-color');
 
-const test = () => {
-	console.log('this is a test');
-};
+
+navToggle.addEventListener('click', () => {
+  document.body.classList.toggle('nav-open');
+  nav.classList.toggle('vissible');
+
+color.style.background= "white";
+  
+});
