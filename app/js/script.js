3665352b18cefdbdc8cb578382f50bae6ccf7395
Marko djuric <maximumspid@gmail.com>
2022-05-08 21:49:36 +0200

    styling menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,14 +1,24 @@
 const navToggle = document.querySelector('.header__toggle');
 const nav = document.querySelector('.nav-side');
 const hamburger = document.querySelector('.hamburger');
-let hamBef = window.getComputedStyle(hamburger, '::before');
-let color = style.getPropertyValue('background-color');
+let hamBef = document.querySelector('.hamburger');
+let style = window.getComputedStyle(hamBef, '::before');
+let x = true;
 
+//Click Event
+  navToggle.addEventListener('click', function() {
 
-navToggle.addEventListener('click', () => {
-  document.body.classList.toggle('nav-open');
-  nav.classList.toggle('vissible');
-
-color.style.background= "white";
+    document.body.classList.toggle('nav-open');
+    nav.classList.toggle('vissible');
   
-});
+  
+    if (x===true) {
+    
+      hamBef.style.setProperty('--san-juan-blue', 'white');
+      x = false;
+    }else {
+  
+    hamBef.style.setProperty('--san-juan-blue', 'hsl(207, 33%, 32%)');
+    x = true;
+    }
+  });
