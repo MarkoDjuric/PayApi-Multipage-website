6656fa6593c7c6235cc77d3d5759dbb0685a643f
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2022-05-20 08:46:17 +0200

    Delete dist directory

--- dist/script.js
+++
@@ -1,2 +0,0 @@
-"use strict";var navToggle=document.querySelector(".header__toggle"),nav=document.querySelector(".nav-side"),hamburger=document.querySelector(".hamburger"),hamBef=document.querySelector(".hamburger"),style=window.getComputedStyle(hamBef,"::before"),x=!0;navToggle.addEventListener("click",(function(){document.body.classList.toggle("nav-open"),nav.classList.toggle("vissible"),!0===x?(hamBef.style.setProperty("--san-juan-blue","white"),x=!1):(hamBef.style.setProperty("--san-juan-blue","hsl(207, 33%, 32%)"),x=!0)}));
-//# sourceMappingURL=script.js.map
